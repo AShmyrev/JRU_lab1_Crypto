08f592002a8afd80081cdbadf6b560281bf53a77
shmyrevandrey <shmyrevandrey@gmail.com>
2022-01-29 22:55:41 +0300

    1.2 Added encrypt() and decrypt() methods with Scanner problem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        startProgram();
+        startProgram();
 
-        String testText = "    Это    тестовая строка, которая должна быть зашифрована, и, в последствии, расшифрована. a eto doljno otbrositsya.   ";
-        int testKey = 5/*Utils.ALPHABET.length()*/;
-        System.out.println(testText);
-        String test = new String(Utils.prepareUserText(testText));
-        System.out.println(test);
+
+
+//        String testText = "    Это    тестовая строка, которая должна быть зашифрована, и, в последствии, расшифрована. a eto doljno otbrositsya.   ";
+//        int testKey = 5/*Utils.ALPHABET.length()*/;
+//
+//        System.out.println(Utils.encrypt(testText, testKey));
+
+
+//        System.out.println(testText);
+//        String test = new String(Utils.prepareUserText(testText));
+//        System.out.println(test);
+
+
 
 
         /*
@@ -23,7 +31,6 @@ public class Main {
          *       не больше 4 гласных/согласных подряд
          *       '.' ' ' 'A'
          *   4.  Статистический Анализ
-         *       Можно попробовать взять Thinking in java
          *       Ключ не ищется! Берется зашифрованный файл и расшифрованный аналогичный файл того же автора
          *       и происходит попытка расшифровки на основе добавочного файла.
          */
@@ -35,31 +42,34 @@ public class Main {
                     Welcome to my humble crypto program.
                     Choose necessary option from the list:
                     1  -   encrypt selected text via key
-                    2  -   decrypt selected text with bruteforce method
-                    3  -   decrypt selected text with statistic analysis method
+                    2  -   decrypt selected text via key
+                    3  -   decrypt selected text with bruteforce method
+                    4  -   decrypt selected text with statistic analysis method
                     0  -   exit program""");
 
-            int userOption = Utils.scanner.nextInt();
+            int userOption = Utils.SCANNER.nextInt();
             switch (userOption) {
                 case (1): {
-                    System.out.println("Put the full path of your text here: ");
-                    String textPath = Utils.scanner.nextLine();
-                    System.out.println("Put a key from 0 to " + Utils.ALPHABET_SET.size());
-                    int userKey = Utils.scanner.nextInt();
-                    Utils.textWriterToFile(Utils.encrypt(Utils.textReaderFromFile(textPath), userKey));
-                    System.out.println("The text encrypted to "/*TODO: create Path*/);
+                    Utils.encrypt();
                     break;
                 }
                 case (2): {
-                    System.out.println("bruteforce decrypt option");
+                    Utils.decrypt();
                     break;
                 }
                 case (3): {
+                    System.out.println("bruteforce decrypt option");
+                    break;
+                }
+                case (4): {
                     System.out.println("statistic analysis option");
                     break;
                 }
                 case (0): {
                     System.exit(0);
+                }
+                default: {
+                    System.out.println("Wrong number!");
                 }
             }
         }
--- src/com/company/Utils.java
+++ src/com/company/Utils.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Set;
@@ -9,22 +12,80 @@ public class Utils {
     public static final Set<Character> ALPHABET_SET = Set.of(
             'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
             'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
-            '.', ',', '"', ':', '-', '!', '?', ' ',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '.', ',', '"', ':', ';', '-', '—', '!', '?', ' ', '/', '\\',
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-            );
-    public static final Scanner scanner = new Scanner(System.in);
+    );
+    public static final String RUSSIAN_ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    public static final String ENGLISH_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    public static final String SYMBOLS = ".,\":-—!? /\\";
+    public static final String DIGITS = "0123456789";
+    public static final String ALPHABET = RUSSIAN_ALPHABET + ENGLISH_ALPHABET +  SYMBOLS + DIGITS;
+    public static final Scanner SCANNER = new Scanner(System.in);
+    public static final int TEST_KEY = 3;
 
-    // TODO: Расшифровывает текст возвращая ключ.
-    public static int decrypt(String text) {
-        return 0;
-    }
-    // TODO: Зашифровывает текст по заданному ключу.
-    public static String encrypt(String text, int key) {
+
+    // TODO: Расшифровывает текст на основе статистического анализа
+    public static String decryptStatisticAnalysis(String text, String similarText) {
         return "";
     }
+    // TODO: Расшифровывает текст методом bruteforce
+    public static String decryptBruteForce(String text) {
+        return "";
+    }
+    /** Расшифровывает текст по ключу. */
+    // TODO: запись/чтение из файла. Проблема со Scanner
+    public static void decrypt() {
+        String userText = /*getUserTextFromFile()*/"вaхс0хзфхсегc0фхуснг-0нсхсугc0жсойрг0дюхя0кгылчусегрг-0л-0е0тсфозжфхелл-0угфылчусегрг:0d0hwr0gromqr0rweurvlwv,d:";
+        String preparedUserText = prepareUserText(userText);
 
-    /** Удаление символов, не входящих в алфавит.*/
+        System.out.println("Put a key from 0 to " + Utils.ALPHABET_SET.size());
+        int userKey = Utils.SCANNER.nextInt();
+
+        char[] userTextChars = preparedUserText.toCharArray();
+        char[] alphabetChars = ALPHABET.toCharArray();
+        for (int i = 0; i < userTextChars.length; i++) {
+            for (int j = 0; j < alphabetChars.length; j++) {
+                if (userTextChars[i] == alphabetChars[j]) {
+                    int newPosition = j - TEST_KEY;
+                    if (newPosition < 0) {
+                        newPosition = alphabetChars.length + newPosition;
+                    }
+                    userTextChars[i] = alphabetChars[newPosition];
+                    break;
+                }
+            }
+        }
+    }
+
+    /** Зашифровывает текст по заданному ключу. */
+    // TODO: запись/чтение из файла. Проблема со Scanner
+    public static void encrypt(/*String userText, int userKey*/) {
+        System.out.println("Put the full path of your text here: ");
+        String userTextPath = Utils.SCANNER.nextLine();
+
+        String userText = /*getUserTextFromFile(userTextPath)*/"    9Это    тестовая строка, которая должна быть зашифрована, и, в последствии, расшифрована. a eto doljno otbrositsya.   ";
+        String preparedUserText = prepareUserText(userText);
+
+        System.out.println("Put a key from 0 to " + Utils.ALPHABET_SET.size());
+        int userKey = Utils.SCANNER.nextInt();
+
+        char[] userTextChars = preparedUserText.toCharArray();
+        char[] alphabetChars = ALPHABET.toCharArray();
+        for (int i = 0; i < userTextChars.length; i++) {
+            for (int j = 0; j < alphabetChars.length; j++) {
+                if (userTextChars[i] == alphabetChars[j]) {
+                    int newPosition = j + TEST_KEY;
+                    userTextChars[i] = alphabetChars[newPosition % alphabetChars.length];
+                    System.out.print(userTextChars[i]);
+                    break;
+                }
+            }
+        }
+        writeTextToFile(new String(userTextChars), userTextPath);
+    }
+
+    /** Удаление символов, не входящих в алфавит. */
     public static String prepareUserText(String userText) {
         userText = userText.toLowerCase();
         userText = userText.trim();
@@ -40,7 +101,7 @@ public class Utils {
         checkedTextChars = removeSpaces(checkedTextChars);
         return getStringFromArrayListCharacter(checkedTextChars);
     }
-    /** Получает String из ArrayList<Character>*/
+    /** Получает String из ArrayList<Character> */
     public static String getStringFromArrayListCharacter(ArrayList<Character> list) {
         StringBuilder builder = new StringBuilder(list.size());
         for(Character ch: list)
@@ -50,7 +111,7 @@ public class Utils {
         return builder.toString();
     }
 
-    /** Удаляет двойные пробелы, которые могли возникнуть из-за очистки текста от лишних символов*/
+    /** Удаляет двойные пробелы, которые могли возникнуть из-за очистки текста от лишних символов */
     public static ArrayList<Character> removeSpaces(ArrayList<Character> checkedTextChars) {
         ArrayList<Character> checkedTextCharsWithoutDoubledSpaces = new ArrayList<>();
 
@@ -65,13 +126,26 @@ public class Utils {
         return checkedTextCharsWithoutDoubledSpaces;
     }
 
-    // TODO: Записывает текст из строки в файл.
-    public static void textWriterToFile(String text) {
-
+    /** Записывает текст из строки в файл. */
+    public static void writeTextToFile(String userText, String previousUserTextPath) {
+        int txtIndex = previousUserTextPath.lastIndexOf(".txt");
+        String fileName = previousUserTextPath.substring(0, txtIndex);
+        String newFileName = fileName + "-encrypted" + ".txt";
+        try {
+            Files.writeString(Path.of(newFileName), userText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    // TODO: Считывает текст из файла в строку.
-    public static String textReaderFromFile(String stringPath) {
-        return "";
+    /** Считывает текст из файла в строку. */
+    public static String getUserTextFromFile(String userTextPath) {
+        try {
+            byte[] userTextBytes = Files.readAllBytes(Path.of(userTextPath));
+            return new String(userTextBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 
@@ -81,11 +155,11 @@ public class Utils {
     /**
      * Deprecated methods
      */
-    // public static final String RUSSIAN_ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-    //    public static final String ENGLISH_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-    //    public static final String SYMBOLS = ".,\":-!? ";
-    //    public static final String DIGITS = "0123456789";
-    //    public static final String ALPHABET = RUSSIAN_ALPHABET + /*ENGLISH_ALPHABET +  */SYMBOLS/* + DIGITS*/;
+    //     public static final String RUSSIAN_ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+//        public static final String ENGLISH_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+//        public static final String SYMBOLS = ".,\":-!? ";
+//        public static final String DIGITS = "0123456789";
+//        public static final String ALPHABET = RUSSIAN_ALPHABET + /*ENGLISH_ALPHABET +  */SYMBOLS/* + DIGITS*/;
     /** DEPRECATED Проверяет на наличие в тексте символов из алфавита.
     public static String deprecatedCheckTextAccordingToTheAlphabet(String text) {
         text = text.toLowerCase();
