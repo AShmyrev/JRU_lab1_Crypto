4e1891e05c4e93bb674b215036012e74c8afaaf6
shmyrevandrey <shmyrevandrey@gmail.com>
2022-01-30 14:38:37 +0300

    1.3 Added methods which work with files. Scanner problem has been almost resolved

--- src/com/company/Utils.java
+++ src/com/company/Utils.java
@@ -22,7 +22,10 @@ public class Utils {
     public static final String DIGITS = "0123456789";
     public static final String ALPHABET = RUSSIAN_ALPHABET + ENGLISH_ALPHABET +  SYMBOLS + DIGITS;
     public static final Scanner SCANNER = new Scanner(System.in);
+
     public static final int TEST_KEY = 3;
+    public static final String TEST_USER_PATH = "C:\\Users\\User\\Desktop\\testUserFile.txt";
+    public static final String TEST_USER_PATH_ENCRYPTED = "C:\\Users\\User\\Desktop\\testUserFile-encrypted.txt";
 
 
     // TODO: Расшифровывает текст на основе статистического анализа
@@ -33,10 +36,11 @@ public class Utils {
     public static String decryptBruteForce(String text) {
         return "";
     }
+
     /** Расшифровывает текст по ключу. */
-    // TODO: запись/чтение из файла. Проблема со Scanner
     public static void decrypt() {
-        String userText = /*getUserTextFromFile()*/"вaхс0хзфхсегc0фхуснг-0нсхсугc0жсойрг0дюхя0кгылчусегрг-0л-0е0тсфозжфхелл-0угфылчусегрг:0d0hwr0gromqr0rweurvlwv,d:";
+        String userTextPath = getUserTextPath();
+        String userText = getUserTextFromFile(userTextPath)/*"вaхс0хзфхсегc0фхуснг-0нсхсугc0жсойрг0дюхя0кгылчусегрг-0л-0е0тсфозжфхелл-0угфылчусегрг:0d0hwr0gromqr0rweurvlwv,d:"*/;
         String preparedUserText = prepareUserText(userText);
 
         System.out.println("Put a key from 0 to " + Utils.ALPHABET_SET.size());
@@ -47,7 +51,7 @@ public class Utils {
         for (int i = 0; i < userTextChars.length; i++) {
             for (int j = 0; j < alphabetChars.length; j++) {
                 if (userTextChars[i] == alphabetChars[j]) {
-                    int newPosition = j - TEST_KEY;
+                    int newPosition = j - userKey;
                     if (newPosition < 0) {
                         newPosition = alphabetChars.length + newPosition;
                     }
@@ -56,15 +60,14 @@ public class Utils {
                 }
             }
         }
+        writeTextToFile(new String(userTextChars), userTextPath, false);
     }
 
     /** Зашифровывает текст по заданному ключу. */
-    // TODO: запись/чтение из файла. Проблема со Scanner
-    public static void encrypt(/*String userText, int userKey*/) {
-        System.out.println("Put the full path of your text here: ");
-        String userTextPath = Utils.SCANNER.nextLine();
+    public static void encrypt() {
+        String userTextPath = getUserTextPath();
 
-        String userText = /*getUserTextFromFile(userTextPath)*/"    9Это    тестовая строка, которая должна быть зашифрована, и, в последствии, расшифрована. a eto doljno otbrositsya.   ";
+        String userText = getUserTextFromFile(userTextPath)/*"    9Это    тестовая строка, которая должна быть зашифрована, и, в последствии, расшифрована. a eto doljno otbrositsya.   "*/;
         String preparedUserText = prepareUserText(userText);
 
         System.out.println("Put a key from 0 to " + Utils.ALPHABET_SET.size());
@@ -75,16 +78,33 @@ public class Utils {
         for (int i = 0; i < userTextChars.length; i++) {
             for (int j = 0; j < alphabetChars.length; j++) {
                 if (userTextChars[i] == alphabetChars[j]) {
-                    int newPosition = j + TEST_KEY;
+                    int newPosition = j + userKey;
                     userTextChars[i] = alphabetChars[newPosition % alphabetChars.length];
                     System.out.print(userTextChars[i]);
                     break;
                 }
             }
         }
-        writeTextToFile(new String(userTextChars), userTextPath);
+        writeTextToFile(new String(userTextChars), userTextPath, true);
     }
 
+
+    /** Получает путь к файлу пользователя */
+    // TODO: Исправить двойной вызов Scanner
+    private static String getUserTextPath() {
+        System.out.println("Put the full path of your text here: ");
+        try {
+            String userTextPath = Utils.SCANNER.nextLine();
+            while (!userTextPath.endsWith(".txt")) {
+                System.out.println("There is a mistake in the filename. Try again: ");
+                userTextPath = Utils.SCANNER.nextLine();
+            }
+            return userTextPath;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     /** Удаление символов, не входящих в алфавит. */
     public static String prepareUserText(String userText) {
         userText = userText.toLowerCase();
@@ -102,7 +122,7 @@ public class Utils {
         return getStringFromArrayListCharacter(checkedTextChars);
     }
     /** Получает String из ArrayList<Character> */
-    public static String getStringFromArrayListCharacter(ArrayList<Character> list) {
+    private static String getStringFromArrayListCharacter(ArrayList<Character> list) {
         StringBuilder builder = new StringBuilder(list.size());
         for(Character ch: list)
         {
@@ -110,9 +130,8 @@ public class Utils {
         }
         return builder.toString();
     }
-
     /** Удаляет двойные пробелы, которые могли возникнуть из-за очистки текста от лишних символов */
-    public static ArrayList<Character> removeSpaces(ArrayList<Character> checkedTextChars) {
+    private static ArrayList<Character> removeSpaces(ArrayList<Character> checkedTextChars) {
         ArrayList<Character> checkedTextCharsWithoutDoubledSpaces = new ArrayList<>();
 
         checkedTextCharsWithoutDoubledSpaces.add(checkedTextChars.get(0));
@@ -125,12 +144,17 @@ public class Utils {
         }
         return checkedTextCharsWithoutDoubledSpaces;
     }
-
-    /** Записывает текст из строки в файл. */
-    public static void writeTextToFile(String userText, String previousUserTextPath) {
+    /** Записывает текст из строки в файл. false - зашифрованный, true - расшифрованный*/
+    public static void writeTextToFile(String userText, String previousUserTextPath, boolean isEncryptOrDecrypt) {
         int txtIndex = previousUserTextPath.lastIndexOf(".txt");
         String fileName = previousUserTextPath.substring(0, txtIndex);
-        String newFileName = fileName + "-encrypted" + ".txt";
+        String[] methods = new String[] {"-encrypted", "-decrypted"};
+        String newFileName = fileName;
+        if (isEncryptOrDecrypt) {
+            newFileName = newFileName + methods[0] + ".txt";
+        } else {
+            newFileName = newFileName + methods[1] + ".txt";
+        }
         try {
             Files.writeString(Path.of(newFileName), userText);
         } catch (IOException e) {
